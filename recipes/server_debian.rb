98360e2cd5433c5ae54b480b8d8883ce0692af8c
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-01 16:41:20 +0200

    Prefer using attribute for pg data folder in server_debian recipe

--- recipes/server_debian.rb
+++ recipes/server_debian.rb
@@ -22,7 +22,7 @@ package node['postgresql']['server']['packages']
 execute 'Set locale and Create cluster' do
   command 'export LC_ALL=C; /usr/bin/pg_createcluster ' + node['postgresql']['version'] + ' main'
   action :run
-  not_if { ::File.directory?('/var/lib/postgresql/' + node['postgresql']['version'] + '/main') }
+  not_if { ::File.directory?(node['postgresql']['config']['data_directory']) }
 end
 
 include_recipe 'postgresql::server_conf'
